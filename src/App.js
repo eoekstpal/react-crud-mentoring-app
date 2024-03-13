f4280016c35b2b8b8a1575acfda0916c412b5cc7
eoekstpal <park.se.mi47@gmail.com>
2024-03-14 04:38:05 +0900

    edit, delete 기능 추가, 오류 수정해야함

--- src/App.js
+++ src/App.js
@@ -1,45 +1,66 @@
 import { useState } from 'react';
 import './App.css';
+import uuid from 'react-uuid';
 
 function App() {
-  const [data, setData] = useState([]); //모든 데이터
-  const [inputs, setInputs] = useState(''); //input 데이터
-  const [edit, setEdit] = useState('');
+  const [data, setData] = useState([]); // 모든 데이터
+  const [inputs, setInputs] = useState(''); // input 데이터
+  const [edit, setEdit] = useState(''); // 수정한 input 데이터
 
   // 입력한 데이터
   const handleInput = (e) => {
     setInputs(e.target.value);
-    console.log(e.target.value)
   }
   // 입력한 데이터 가져오기
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     // const newData = {id: crypto.randomUUID(), data};
     // const newDatas = [...newDatas, newData]
+    // e.preventDefault();
     let newData = []
     for (let i = 0; i < data.length; i++) {
       newData = [ ...newData, data[i] ]
     }
-    newData = [ ...newData, { id: crypto.randomUUID(), text: inputs, edit: false}]
-    setInputs(data)
+    newData = [ ...newData, { id: uuid(), text: inputs, editstate: false}]
     setData(newData)
-    console.log(inputs)
+    // !!!!!! 버튼 클릭시 input칸 내용 삭제 안됨 !!!!!!
+     // setInputs('')
     console.log(newData)
   }
-  // 데이터 수정 하기
-  const handleInputEdit(e) => {
-
+  // 수정할 데이터
+  function handleInputEdit(e){
+    setEdit(e.target.value)
   }
-  // 수정 btn
-  const handleEditDataBtn(data) => {
-
+  // 데이터 수정하기
+  function handleEditDataBtn(id) {
+    // const editdata = data.find(data => data.id === id)
+    let newArr = []
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id === id){
+        newArr = [ ...newArr, { id: id, text: edit, editstate: false}]
+      } else {
+        newArr = [ ...newArr, data[i]]
+      }
+      setData(newArr)
+    }
   }
 
-  const handleEditUndo() => {
-    
+  function handleEditUndo(inputdata) {
+    let newArr = []
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].id === inputdata.id) {
+        if (inputdata.editstate === false) { // editstate true로 바꿔서 edit 할 수 있도록
+          newArr = [ ...newArr, {id: inputdata.id, test: inputdata.text, editstate: true}]
+        } else { // // editstate true로 바꿔서 edit 종료
+          newArr = [ ...newArr, {id: inputdata.id, test: inputdata.text, editstate: false}]
+        }
+      } else {
+        newArr = [ ...newArr, data[i]]
+      } setData(newArr)
+    }
   }
 
-  const handleDelete() => {
-
+  function handleDelete(id) {
+    setData(data.filter(data => data.id !== id))
   }
 
   return (
@@ -51,19 +72,21 @@ function App() {
       </div>
       
       <div>
-        {data.map((data, i) => (
+        {data.map((data) => (
           <div>
-            {data.edit === true ? //edit === true면 수정할 수 있도록 textarea 띄움
+            {data.editstate === true ? //edit === true면 수정할 수 있도록 textarea 띄움
               <div>
+                {/* !!!!!! 글 수정하기시 기존 글이 안불러옴(빈칸) 
+                취소 누르면 빈칸으로 다시 수정되어 입력됨 !!!!!! */}
                 <textarea onChange={handleInputEdit}>{data.text}</textarea>
-                <button onClick={(e) => {handleEditDataBtn(data,e)}}>수정완료</button>
+                <button onClick={(e) => {handleEditDataBtn(data.id, e)}}>수정완료</button>
                 <button onClick={(e) => {handleEditUndo(data, e)}}>수정취소</button>
               </div>
               : // edit === false면 수정하거나 삭제하도록 함
               <div>
                 <div>{data.text}</div>
                 <button onClick={(e) => {handleEditUndo(data, e)}}>글 수정</button>
-                <button onClick={(e) => handleDelete(data.id, e)}>글 삭제</button>
+                <button onClick={(e) => {handleDelete(data.id, e)}}>글 삭제</button>
               </div>
             }
           </div>
