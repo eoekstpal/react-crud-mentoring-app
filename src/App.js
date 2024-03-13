0339429a8b770215dd6acb4d1d7f5b800c2ed69a
eoekstpal <park.se.mi47@gmail.com>
2024-03-13 22:49:58 +0900

    reset

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import { useState } from 'react';
 import './App.css';
-import TodoList from './components/TodoList';
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: 'hi',
-      checked: true
-    }
-  ])
 
   return (
     <div>
-      <TodoList todos={todos}/>
     </div>
   );
 }
