472f4051ee75daa221e77438ba932983be6f3cbb
eoekstpal <park.se.mi47@gmail.com>
2024-03-13 22:08:59 +0900

    eslint parsing error -> eslintrc 파일 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'hi',
+      checked: true
+    }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoList todos={todos}/>
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoItem = () => {
+  return (
+    <>
+        hi
+    </>
+  )
+}
+
+export default TodoItem
