19570bd15389e01843815f7d5c49633bafcf5d98
eoekstpal <park.se.mi47@gmail.com>
2024-03-14 03:23:38 +0900

    layout 설정

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ function App() {
   const [inputs, setInputs] = useState(''); //input 데이터
   const [edit, setEdit] = useState('');
 
-  const handleOnChange = (e) => {
+  // 입력한 데이터
+  const handleInput = (e) => {
     setInputs(e.target.value);
     console.log(e.target.value)
   }
-
-  const handleOnClick = () => {
+  // 입력한 데이터 가져오기
+  const handleSubmit = () => {
     // const newData = {id: crypto.randomUUID(), data};
     // const newDatas = [...newDatas, newData]
     let newData = []
@@ -24,13 +25,49 @@ function App() {
     console.log(inputs)
     console.log(newData)
   }
+  // 데이터 수정 하기
+  const handleInputEdit(e) => {
+
+  }
+  // 수정 btn
+  const handleEditDataBtn(data) => {
+
+  }
+
+  const handleEditUndo() => {
+    
+  }
+
+  const handleDelete() => {
+
+  }
 
   return (
     <div>
       <h1>CRUD APP</h1>
       <div>
-        <input onChange={handleOnChange} />
-        <button onClick={handleOnClick}>추가</button>
+        <input onChange={handleInput} />
+        <button onClick={handleSubmit}>글 추가</button>
+      </div>
+      
+      <div>
+        {data.map((data, i) => (
+          <div>
+            {data.edit === true ? //edit === true면 수정할 수 있도록 textarea 띄움
+              <div>
+                <textarea onChange={handleInputEdit}>{data.text}</textarea>
+                <button onClick={(e) => {handleEditDataBtn(data,e)}}>수정완료</button>
+                <button onClick={(e) => {handleEditUndo(data, e)}}>수정취소</button>
+              </div>
+              : // edit === false면 수정하거나 삭제하도록 함
+              <div>
+                <div>{data.text}</div>
+                <button onClick={(e) => {handleEditUndo(data, e)}}>글 수정</button>
+                <button onClick={(e) => handleDelete(data.id, e)}>글 삭제</button>
+              </div>
+            }
+          </div>
+        ))}
       </div>
     </div>
   );
